7b54357fe31049a7394417a637eeaadaf001f5e9
Sienna Truong <tqc1234@gmail.com>
2025-08-11 00:19:15 -0400

    fix: forgot to include `IngredientModule`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { RecipeModule } from './recipe/recipe.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { envService } from './env/env.service';
 import { UserModule } from './user/user.module';
+import { IngredientModule } from './ingredient/ingredient.module';
 
 @Module({
   imports: [
+    IngredientModule,
     RecipeModule,
     UserModule,
     
